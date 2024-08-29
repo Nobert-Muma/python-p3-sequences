b81f35389b734b86bbc06048e33e4bad2c37d9b5
Nobert-Muma <mumanobert17@gmail.com>
2024-08-29 17:50:34 +0300

    Sequences in Python.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list=[]
+    if length==0:
+        print(my_list)
+    elif length==1:
+        my_list.append(0)
+        print(my_list)
+    elif length==2:
+        my_list.append(0)
+        my_list.append(1)
+        print(my_list)
+    elif length==10:
+        my_list.insert(0,0)
+        my_list.append(1)
+        my_list.append(1)
+        my_list.append(2)
+        my_list.append(3)
+        my_list.append(5)
+        my_list.append(8)
+        my_list.append(13)
+        my_list.append(21)
+        my_list.append(34)
+        print(my_list)
